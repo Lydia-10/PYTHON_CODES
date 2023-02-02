3e8eda3b38567077a231e76e462c67bb30427329
Lydia-10 <124137916+Lydia-10@users.noreply.github.com>
2023-02-02 15:25:39 +0300

    Add files via upload

---
+++ grosspay.py
@@ -0,0 +1,5 @@
+# calculating the grosspay
+hours=float(input("enter the hours"))
+rate=float(input("enter the rate per hour"))
+grosspay=hours*rate
+print("grosspay is:", grosspay)
---
+++ helloworld.py
@@ -0,0 +1,13 @@
+#program to write hello world
+print("hello world")
+#booleen
+print("true")
+#floating point
+a=46.46
+print(a)
+#string
+name="python"
+print("name")
+#integer
+b=20
+print(b)
