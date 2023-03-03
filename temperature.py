eb726b5107400ed255a304bfd079f5427cc9247a
Lydia Wavinya <124137916+Lydia-10@users.noreply.github.com>
2023-03-03 11:12:43 +0300

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,10 @@
+class Temperature():
+    def_init_(self,t):
+        self.Temperature=t
+        def fahrenheit(self):
+            print(self,temperature*32)
+            def celcius(self):
+                print(self,temperature/32)
+Newtemperature=temperature(64)
+Newtemperature.fahrenheit()
+Newtemperature.celsius()
